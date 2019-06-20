ec1c2189f7a5c4119efd427abd795196f1668096
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-20 18:21:05 -0400

    Add basic tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,7 +10,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize("directory", [{"path": "/var/cisa/example", "mode": "0o755"}])
+def test_packages(host, directory):
+    """Test that the appropriate directories were created."""
+    assert host.file(directory["path"]).exists
+    assert host.file(directory["path"]).is_directory
+    assert oct(host.file(directory["path"]).mode) == directory["mode"]
+
+
+@pytest.mark.parametrize("f", ["/var/cisa/example/docker-compose.yml"])
+def test_command(host, f):
+    """Test that appropriate files exist."""
+    assert host.file(f).exists
+    assert host.file(f).is_file
